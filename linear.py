445e0f37e9502764ef83f70d2b310caefca20449
Matt <matthew.jacobsen@gmail.com>
2014-03-12 09:15:40 -0400

    cleaned up linear regregression and fixed a few small errors in the math

--- linear.py
+++ linear.py
@@ -1,38 +1,28 @@
-# From calculation, we expect that the local minimum occurs at x=9/4
- 
 import matplotlib.pyplot as plt
 import numpy as np
 
-m = 6 # The algorithm starts at x=6
-b = 9
-a = 0.01 # step size
+m = 6.0 # Start the slope at 6
+b = 9.0	# Start the y-intercept at 9
+a = 0.1 # step size
 
 X = [1,2,3]
 Y = [12,8,4]
+M = float(len(X)) if len(X) == len(Y) else sys.exit('X and Y are not the same length')
 
-def J(b, m):
-    summ = 0.0
-    for x,y in zip(X,Y):
-        summ += (b + m*x - y)**2
-    return summ / (len(X))
+def J():
+    return (1/M) * sum((b + m*x - y)**2 for x,y in zip(X,Y))
 
 def dJ_m():
-    summ = 0.0
-    for x,y in zip(X,Y):
-        summ += 2*(b + m*x - y) * x
-    return summ / 2*(len(X))
+    return (1/(2*M)) * sum(2*(b + m*x - y) * x for x,y in zip(X,Y))
 
 def dJ_b():
-    summ = 0.0
-    for x,y in zip(X,Y):
-        summ += 2*(b + m*x - y) * y
-    return summ / 2*(len(X))
+    return (1/(2*M)) * sum(2*(b + m*x - y) for x,y in zip(X,Y))
 
 for _ in xrange(500):
     m -= a * dJ_m()
     b -= a * dJ_b()
-    cost = J(b, m)
-    print cost
+    cost = J()
+    #print cost
 
 print "Local minimum occurs at", m, b
 
